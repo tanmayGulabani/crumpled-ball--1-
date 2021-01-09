2a6c23be459e905f50c7662eac1ec16358533eae
tanmayGulabani <75249050+tanmayGulabani@users.noreply.github.com>
2021-01-09 22:07:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Bo dy;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	dustbin = new Dustbin(720,390,100,10)
+    paper = new Paper(100,300,10)
+	ground = Bodies.rectangle(width/2, 400, width,10)
+	
+	Engine.run(engine);
+
+	{
+		isStatic: true
+	}
+	world.add(world,ground);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  dustbin.display();
+  paper.display();
+
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+    if(keyCode === UP_ARROW){
+        Matter.Body.applyForce(crumpledPaper.body,crumpledPaper.body.position,{x:74,y:-75});
+    }
+}
+
+
+
